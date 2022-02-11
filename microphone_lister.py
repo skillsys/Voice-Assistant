00a5b9952ee502ac5a25c9d97084893174439a96
skillsys <skillsys@protonmail.com>
2022-02-11 23:49:49 +0530

    "Microphone lister changed to use pyaudio instead"

--- microphone_lister.py
+++ microphone_lister.py
@@ -1,2 +1,13 @@
-import speech_recognition as s_r
-print(s_r.Microphone.list_microphone_names())
+from pyaudio import PyAudio
+"""
+Returns a list of the names of all available microphones. For microphones where the name can't be retrieved, the list entry contains ``None`` instead.
+
+The index of each microphone's name is the same as its device index when creating a ``Microphone`` instance - indices in this list can be used as values of ``device_index``.
+"""
+audio = PyAudio()
+try:
+    for i in range(audio.get_device_count()):
+        device_info = audio.get_device_info_by_index(i)
+        print(device_info.get("name"))
+finally:
+    audio.terminate()
